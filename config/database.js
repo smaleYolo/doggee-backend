e8fdd8160b282db8939a84a58efe8b67f9aebd53
smaleYolo <ilia.artamonov@yandex.ru>
2024-08-01 17:14:05 +0300

    Добавлен пул соединений

--- config/database.js
+++ config/database.js
@@ -1,12 +1,18 @@
 const { Sequelize } = require('sequelize');
-const pg = require('pg'); // Импортируйте модуль pg
+const pg = require('pg');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     dialectModule: pg,
     protocol: 'postgres',
-    logging: false // Отключение логов Sequelize
+    logging: false, // Отключение логов Sequelize
+    pool: {
+        max: 10, // Максимальное количество соединений в пуле
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 });
 
 module.exports = sequelize;
